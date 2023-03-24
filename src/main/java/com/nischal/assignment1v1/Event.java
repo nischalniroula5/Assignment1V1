106773e6b6d418ce2c11f73c82fd009b475bb552
Nischal Niroula <officialnischalniroula@gmail.com>
2023-03-24 12:23:04 +1100

    Changed the class from abstractevent to event

---
+++ src/main/java/com/nischal/assignment1v1/Event.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nischal.assignment1v1;
+
+
+public abstract class Event {
+    
+    private int time;
+    
+    
+    //Creating new object
+    ShopModel shopModel = new ShopModel();
+    IScheduler scheduler = new IScheduler();
+    
+    
+    
+    //Initializing Event
+    public void Event (int time){
+        
+    }
+    
+    
+    //Initializing Getter
+    public int getTime(int time){
+        return time;
+    }
+}
--- src/main/java/com/nischal/assignment1v1/ShopModel.java
+++ src/main/java/com/nischal/assignment1v1/ShopModel.java
@@ -1,10 +1,12 @@
 
 package com.nischal.assignment1v1;
 
-/**
- *
- * @author nischalniroula
- */
 public class ShopModel {
+    
+    //Initializing constructor for customer group
+    CustomerGroup customerGroup = new CustomerGroup();
+    
+    
+    //Initializing array list of customer group
     
 }
